02ed0b053d5a753be283bdad86fb83eacf7b9a9e
masonjar <mason.whitaker79@gmail.com>
2022-05-19 08:37:58 -0700

    Began work on getting data from the API, currently incomplete

---
+++ modules/games.js
@@ -0,0 +1,46 @@
+"use strict";
+let cache = require('./cache.js');
+const axios = require("axios");
+module.exports = findGames;
+
+async function findGames(gameName) {
+    const query = gameName
+    const url = `https://api.rawg.io/api/games?key=${process.env.API_KEY}&search="${query}"`
+    console.log(url);
+    if (cache[key] && Date.now() - cache[key].timestamp < 10800000) {
+        console.log("Cache hit");
+      } else {
+        console.log("Cache miss");
+        cache[key] = {};
+        cache[key].timestamp = Date.now();
+        cache[key].data = axios
+          .get(url)
+          .then((response) => getGames(response.data));
+      }
+      console.log("Data: ", cache[key].data);
+      return cache[key].data;
+}
+
+function getGames(gameData) {
+    try {
+      console.log("GameData: ", gameData);
+      const gameSummary = gameData.data.results.map((game) => {
+        return new Forecast(day);
+      });
+      console.log("Data: ", gameSummary);
+      return Promise.resolve(gameSummary);
+    } catch (e) {
+      console.log(e);
+      return Promise.reject(e);
+    }
+  }
+  
+  function Forecast(game) {
+    this.gameName = game.name;
+    this.genres = game.genres;
+    this.gameTags = game.tags;
+    this.platforms = game.platforms;
+    this.backgroundImg = game.background_image;
+    this.gameStores = game.stores;
+    this.release_date = game.released
+  }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const axios = require("axios");
+const findGames = require("./modules/games.js")
 app.use(cors());
 app.use(express.json());
 
@@ -16,6 +17,31 @@ app.get("/test", (request, response) => {
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
+app.get("/games", getGames);
+
+function getGames(request, response) {
+  let search = request.query.gameName
+  
+  findGames(search)
+  .then(gameData => response.send(gameData))
+  .catch( e => {
+    console.error(e)
+    response.status(500)
+    .send("I'm sorry an internal server error occured. Trying again, if the problem persists call support at Fake-Num-Ber")
+  })
+}
+// function weatherHandler(request, response) {
+//   let lat = request.query.lat;
+//   let lon = request.query.lon;
+//   // let searchQuery = request.query.searchQuery;
+
+//  getWeather(lat, lon)
+//   .then(summaries => response.send(summaries))
+//   .catch(error => {
+//     console.log(error);
+//     response.status(500).send('meow meow your request failed.');
+//   })
+// };
 // Mongo database for later
 // const db = mongoose.connection;
 // const Book = require("./models/books");
