35b338261b3855100cddb5aa7007959daae7098b
masonjar <mason.whitaker79@gmail.com>
2022-05-24 08:22:21 -0700

    Began work on database connections

--- server.js
+++ server.js
@@ -2,20 +2,26 @@
 
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const db = mongoose.connection;
 const app = express();
 const axios = require("axios");
 const findGames = require("./modules/games.js")
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
+
+mongoose.connect(process.env.MONGO_CONNECT);
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", (_) => {
+  console.log("Mongo Atlas connection sucessful");
+});
 
 app.get("/test", (request, response) => {
     response.send("test request received");
   });
-
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 app.get("/games", getGames);
 
@@ -31,20 +37,10 @@ function getGames(request, response) {
     .send("I'm sorry an internal server error occured. Trying again, if the problem persists call support at Fake-Num-Ber.")
   })
 }
-// function weatherHandler(request, response) {
-//   let lat = request.query.lat;
-//   let lon = request.query.lon;
-//   // let searchQuery = request.query.searchQuery;
 
-//  getWeather(lat, lon)
-//   .then(summaries => response.send(summaries))
-//   .catch(error => {
-//     console.log(error);
-//     response.status(500).send('meow meow your request failed.');
-//   })
-// };
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
 // Mongo database for later
-// const db = mongoose.connection;
+
 // const Book = require("./models/books");
 
 // mongoose.connect(process.env.MONGO_CONNECT);
